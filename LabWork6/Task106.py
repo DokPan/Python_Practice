c8711db7ad7e62345a56b3bc11d5d1d79a63517f
Alina <alinaroshchina5@gmail.com>
2026-03-25 13:59:19 +0300

    Add files via upload

---
+++ LabWork6/Task106.py
@@ -0,0 +1,6 @@
+try:
+    a=float(input("a = "))
+    b=float(input("b = "))
+    print(a/b)
+except:
+    print("Делить на ноль нельзя")
---
+++ LabWork6/Task206.py
@@ -0,0 +1,7 @@
+a = float(input("a = "))
+while True:
+    b = float(input("b = "))
+    if b !=0:
+        break
+    print("Делить на ноль нельзя")
+print(a/b)
