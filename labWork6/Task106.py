03fb271cd8294572793dba50bac5f80f1c1104c2
Alina <alinaroshchina5@gmail.com>
2026-03-25 13:59:00 +0300

    Delete labWork6 directory

--- labWork6/Task106.py
+++
@@ -1,6 +0,0 @@
-try:
-    a=float(input("a = "))
-    b=float(input("b = "))
-    print(a/b)
-except:
-    print("Делить на ноль нельзя")
--- labWork6/Task206.py
+++
@@ -1,7 +0,0 @@
-a = float(input("a = "))
-while True:
-    b = float(input("b = "))
-    if b !=0:
-        break
-    print("Делить на ноль нельзя")
-print(a/b)
